7d05ca9f04f0ffb842f6306a92cfa8c15679c4fc
Arin <arin.apichitchon@gmail.com>
2024-10-20 17:42:59 -0400

    layouts

---
+++ larazillow11/resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,10 @@
+<template>
+    <Link href="/">Main Page</Link>&nbsp;
+    <Link href="/hello">Show Page</Link>
+    <!-- default keyword flag -->
+    <slot>Default</slot>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+</script>
--- larazillow11/resources/js/Pages/Index/Show.vue
+++ larazillow11/resources/js/Pages/Index/Show.vue
@@ -1,10 +1,13 @@
 <template>
-    <div>
-        show
-    </div>
-    <Link href="/">Index Page</Link>
+    <MainLayout>
+        <div>
+            show
+        </div>
+        <Link href="/">Index Page</Link>
+    </MainLayout>
 </template>
 
 <script setup>
 import { Link } from '@inertiajs/vue3'
+import MainLayout from '../../Layouts/MainLayout.vue';
 </script>
