bb95b6e62245a008fe521c64327079fcdf917ace
Arin <arin.apichitchon@gmail.com>
2024-11-09 21:44:46 -0500

    add --resource ListingController, add route

--- larazillow11/routes/web.php
+++ larazillow11/routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\ListingController;
 
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/hello', [IndexController::class, 'show']);
+
+Route::resource('listings', ListingController::class)->only(['index', 'show']);
